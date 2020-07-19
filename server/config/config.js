b0c69ece6db8c591af6d26f2db8628e1ff8e0f1b
Elias Roussos <eliasIsaiah@outlook.com>
2020-07-19 00:01:15 -0400

    updated config.js

--- server/config/config.js
+++ server/config/config.js
@@ -1,12 +1,20 @@
 require("dotenv").config();
 
 module.exports = {
+  // "development": {
+  //   "use_env_variable": "JAWSDB_URL",
+  //   "dialect": "mysql"
+  // },
   "development": {
-    "use_env_variable": "JAWSDB_URL",
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DB_SCHEMA,
+    "host": process.env.DB_HOST, 
     "dialect": "mysql"
   },
   "test": {
     "use_env_variable": "JAWSDB_URL",
+
     "dialect": "mysql"
   },
   "production": {
